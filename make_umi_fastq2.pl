2e06e76f3fbc2b160028853305e921eccf39af8f
Alexander Predeus <predeus@gmail.com>
2024-08-16 17:25:27 +0100

    Fixes for CR/SR BAM file

--- make_umi_fastq2.pl
+++ make_umi_fastq2.pl
@@ -8,8 +8,8 @@ use strict;
 use warnings;
 
 if (scalar @ARGV != 2) { 
-	print STDERR "USAGE: ./make_umi_fastq.pl <STARsolo or Cell/Space Ranger directory> <output file>\n"; 
-	exit 1; 
+  print STDERR "USAGE: ./make_umi_fastq.pl <STARsolo or Cell/Space Ranger directory> <output file>\n"; 
+  exit 1; 
 } 
 
 my $dir = shift @ARGV;
@@ -18,132 +18,142 @@ my $out = shift @ARGV;
 open OUT,">",$out or die "$!"; 
 
 if (-d $dir."/output" && -e $dir."/Log.final.out") { 
-	print STDOUT "make_umi_fastq.pl: sample was determined to be STARsolo output! \nMaking a single-end, UMI-tools-formatted fastq file..\n";
-	
-	## we need to check if the file was mapped as paired-end; this changes what R1 and R2 actually are.
-	## if $paired == 1, mate1 contains the barcode + UMI; if $paired == 0, it's mate2.  
-	my $paired = 0;
-	open LOG,"<",$dir."/Log.out" or die "$!"; 
-	while (<LOG>) { 
-		$paired = 1 if (m/clip5pNbases 39 0/); 
-	}
-	close LOG;
-	if ($paired) { 
-		print STDOUT "make_umi_fastq.pl: sample was determined to be 5' 10x paired-end experiment!\n";
-	} else { 
-		print STDOUT "make_umi_fastq.pl: sample was determined to be 3'/5' 10x single-end experiment!\n"; 
-	} 
+  print STDOUT "make_umi_fastq.pl: sample was determined to be STARsolo output! \nMaking a single-end, UMI-tools-formatted fastq file..\n";
+  
+  ## we need to check if the file was mapped as paired-end; this changes what R1 and R2 actually are.
+  ## if $paired == 1, mate1 contains the barcode + UMI; if $paired == 0, it's mate2.  
+  my $paired = 0;
+  open LOG,"<",$dir."/Log.out" or die "$!"; 
+  while (<LOG>) { 
+    $paired = 1 if (m/clip5pNbases 39 0/); 
+  }
+  close LOG;
+  if ($paired) { 
+    print STDOUT "make_umi_fastq.pl: sample was determined to be 5' 10x paired-end experiment!\n";
+  } else { 
+    print STDOUT "make_umi_fastq.pl: sample was determined to be 3'/5' 10x single-end experiment!\n"; 
+  } 
 
-	## now do the processing. We keep only the BC+UMI part of the barcode read
-	## this would lose some biological info in case of PE 5' experiments, but I think it should not matter this much for bacterial reads
-	my ($R1,$R2) = ('') x 2;
-	if (-e $dir."/Unmapped.out.mate1.gz" && -e $dir."/Unmapped.out.mate2.gz") { 
-	  $R1 = ($paired) ? $dir."/Unmapped.out.mate1.gz" : $dir."/Unmapped.out.mate2.gz"; 
-	  $R2 = ($paired) ? $dir."/Unmapped.out.mate2.gz" : $dir."/Unmapped.out.mate1.gz";
-	} elsif (-e $dir."/Unmapped.out.mate1.bz2" && -e $dir."/Unmapped.out.mate2.bz2") {
-	  $R1 = ($paired) ? $dir."/Unmapped.out.mate1.bz2" : $dir."/Unmapped.out.mate2.bz2"; 
-	  $R2 = ($paired) ? $dir."/Unmapped.out.mate2.bz2" : $dir."/Unmapped.out.mate1.bz2";
-	} else { 
-		print STDERR "make_umi_fastq.pl: cannot find unmapped read files Unmapped.out.mate(1/2).<gz/bz2> in the STARsolo directory! Exiting .."; 
-		exit 1; 
-	}
+  ## now do the processing. We keep only the BC+UMI part of the barcode read
+  ## this would lose some biological info in case of PE 5' experiments, but I think it should not matter this much for bacterial reads
+  my ($R1,$R2) = ('') x 2;
+  if (-e $dir."/Unmapped.out.mate1.gz" && -e $dir."/Unmapped.out.mate2.gz") { 
+    $R1 = ($paired) ? $dir."/Unmapped.out.mate1.gz" : $dir."/Unmapped.out.mate2.gz"; 
+    $R2 = ($paired) ? $dir."/Unmapped.out.mate2.gz" : $dir."/Unmapped.out.mate1.gz";
+  } elsif (-e $dir."/Unmapped.out.mate1.bz2" && -e $dir."/Unmapped.out.mate2.bz2") {
+    $R1 = ($paired) ? $dir."/Unmapped.out.mate1.bz2" : $dir."/Unmapped.out.mate2.bz2"; 
+    $R2 = ($paired) ? $dir."/Unmapped.out.mate2.bz2" : $dir."/Unmapped.out.mate1.bz2";
+  } else { 
+    print STDERR "make_umi_fastq.pl: cannot find unmapped read files Unmapped.out.mate(1/2).<gz/bz2> in the STARsolo directory! Exiting .."; 
+    exit 1; 
+  }
 
-  print STDOUT "Using unmapped STARsolo files:\nR1 = $R1\nR2 = $R2\n"; 	
+  print STDOUT "Using unmapped STARsolo files:\nR1 = $R1\nR2 = $R2\n";   
   ## this occasionally stupid concoction reads 2 fastq files synchronously.
-	if ($R1 =~ m/gz$/ && $R2 =~ m/gz$/) { 
-		open READ1,"pigz -cd $R1 |" or die "$!"; 
-		open READ2,"pigz -cd $R2 |" or die "$!"; 
-	} else { 
-		open READ1,"lbzip2 -cd $R1 |" or die "$!"; 
-		open READ2,"lbzip2 -cd $R2 |" or die "$!";
-	}
+  if ($R1 =~ m/gz$/ && $R2 =~ m/gz$/) { 
+    open READ1,"pigz -cd $R1 |" or die "$!"; 
+    open READ2,"pigz -cd $R2 |" or die "$!"; 
+  } else { 
+    open READ1,"lbzip2 -cd $R1 |" or die "$!"; 
+    open READ2,"lbzip2 -cd $R2 |" or die "$!";
+  }
 
-	my $nr = 1; 
+  my $nr = 1; 
   my ($rname,$bc,$umi,$seq,$qual) = ('') x 5;
-	my ($passed,$discarded) = (0) x 2; 
-	
-	while (<READ1>) {
-		my $r1 = $_; 
-		chomp $r1;
-		my $r2 = <READ2>;
-		chomp $r2;
+  my ($passed,$discarded) = (0) x 2; 
+  
+  while (<READ1>) {
+    my $r1 = $_; 
+    chomp $r1;
+    my $r2 = <READ2>;
+    chomp $r2;
 
-		if ($nr % 4 == 1) {
-		  $r1 =~ m/(.*?)\s/;                                     ## shortest string until space 
-			$rname = $1;
-			$r2 =~ m/(.*?)\s/; 
-			my $rname2 = $1; 
-			print STDERR "make_umi_fastq.pl: WARNING - read names do not match: $rname =/= $rname2!\n" if ($rname ne $rname2);
-		} elsif ($nr % 4 == 2) { 
-			my $len = length $r1; 
-			my $bclen = ($len > 24) ? 16 : 14; 
-			my $umilen = ($len > 28) ? 10 : $len - $bclen;         ## it's 10 for 5' PE, 10 for 3' v1(24-14) or v2(26-16), and 12 for 3'v3 (28-16)
-			print STDERR "make_umi_fastq.pl: WARNING - UMI length is not equal to 10 or 12!\n" if ($umilen != 10 && $umilen != 12); 
-			$bc = substr $r1,0,$bclen; 
-			$umi = substr $r1,$bclen,$umilen;
-			$seq = $r2;
-		} elsif ($nr % 4 == 0) { 
-			$qual = $r2;
-			if (! low_complexity($seq)) { 
-			  printf OUT "%s_%s_%s\n%s\n+\n%s\n",$rname,$bc,$umi,$seq,$qual;
-			  $passed++; 
-			} else {
-			  #printf STDOUT "DEBUG: filtered low complexity sequence $seq\n"; 
-		      $discarded++; 
-			}
-		} 
-		$nr++; 
-	}
+    if ($nr % 4 == 1) {
+      $r1 =~ m/(.*?)\s/;                                     ## shortest string until space 
+      $rname = $1;
+      $r2 =~ m/(.*?)\s/; 
+      my $rname2 = $1; 
+      print STDERR "make_umi_fastq.pl: WARNING - read names do not match: $rname =/= $rname2!\n" if ($rname ne $rname2);
+    } elsif ($nr % 4 == 2) { 
+      my $len = length $r1; 
+      my $bclen = ($len > 24) ? 16 : 14; 
+      my $umilen = ($len > 28) ? 10 : $len - $bclen;         ## it's 10 for 5' PE, 10 for 3' v1(24-14) or v2(26-16), and 12 for 3'v3 (28-16)
+      print STDERR "make_umi_fastq.pl: WARNING - UMI length is not equal to 10 or 12!\n" if ($umilen != 10 && $umilen != 12); 
+      $bc = substr $r1,0,$bclen; 
+      $umi = substr $r1,$bclen,$umilen;
+      $seq = $r2;
+    } elsif ($nr % 4 == 0) { 
+      $qual = $r2;
+      if (! low_complexity($seq)) { 
+        printf OUT "%s_%s_%s\n%s\n+\n%s\n",$rname,$bc,$umi,$seq,$qual;
+        $passed++; 
+      } else {
+        #printf STDOUT "DEBUG: filtered low complexity sequence $seq\n"; 
+          $discarded++; 
+      }
+    } 
+    $nr++; 
+  }
   close READ1;
   close READ2;
-	print STDOUT "make_umi_fastq.pl: making UMI-tools formatted reads: outputted $passed reads, discarded $discarded reads because of 50nt+ homopolymers ..\n"; 
+  print STDOUT "make_umi_fastq.pl: making UMI-tools formatted reads: outputted $passed reads, discarded $discarded reads because of extensive homopolymers ..\n"; 
 } else { 
-	## TODO: add read counter, add homopolymer filter for Cell Ranger BAM input
+  ## here we assume it's a Cell/Space Ranger/STARsolo-formatted BAM file instead of the directory 
+  ## single- or paired-end (meaning biological reads), with CB/UB set for all reads that have matched against a barcode (and thus are error-corrected). 
+  ## we will take only unmapped reads (-f4), and only R2 for paired-end reads (-f128)
+  my $bam = $dir;
+  if ($bam =~ m/\.bam$/) { 
+    print STDOUT "make_umi_fastq.pl: sample appears to be to be Cell/Space Ranger/STARsolo BAM! \nMaking a single-end, UMI-tools-formatted fastq file..\n";
+  } else { 
+    print STDERR "make_umi_fastq.pl: ERROR - failed to find a BAM file in the directory provided! Exiting .. \n"; 
+    exit 1; 
+  }
+  print STDOUT "Using files: \nBAM = $bam\n";
 
-	## here we assume the presence of a Cell/Space Ranger/STARsolo-formatted BAM file: 
-	## single- or paired-end (meaning biological reads), with CR and UR set for all reads, and CB/UB for reads that mapped successfully
-	## we will take only unmapped reads (-f4), and only R2 for paired-end reads (-f128)
-	## since all the BAM files are named differently, we would need to do a little sing-and-a-dance first: 
-	my $bam = `find $dir/* | grep -iv atac | grep \"\.bam\$\"`; 
-	chomp $bam; 
-	if (length $bam) { 
-		print STDOUT "make_umi_fastq.pl: sample is assumed to be Cell/Space Ranger/STARsolo BAM! \nMaking a single-end, UMI-tools-formatted fastq file..\n";
-	} else { 
-		print STDERR "make_umi_fastq.pl: ERROR - failed to find a BAM file in the directory provided! Exiting .. \n"; 
-		exit 1; 
-	}
-	print STDOUT "Using files: \nBAM = $bam\n";
+  ## we need to check if the file was mapped as paired-end; this changes what R1 and R2 actually are.
+  ## if $paired == 1, flags 64/128 will be set (R1 will be trimmed for BC+UMI in a typical 10x Chromium 5' PE BAM).  
+  my $preads = `samtools view -h $bam | head -10000 | samtools flagstat - | grep \"properly paired\" | cut -d' ' -f1`; 
+  my $paired = ($preads > 0) ? 1 : 0; 
+  if ($paired) { 
+    print STDOUT "make_umi_fastq.pl: sample was determined to be 5' 10x paired-end experiment!\n";
+  } else { 
+    print STDOUT "make_umi_fastq.pl: sample was determined to be 3'/5'/Visium 10x single-end experiment!\n"; 
+  } 
 
-	## we need to check if the file was mapped as paired-end; this changes what R1 and R2 actually are.
-	## if $paired == 1, flags 64/128 will be set (R1 will be trimmed for BC+UMI).  
-	my $preads = `samtools view -h $bam | head -10000 | samtools flagstat - | grep \"properly paired\" | cut -d' ' -f1`; 
-	my $paired = ($preads > 0) ? 1 : 0; 
-	if ($paired) { 
-		print STDOUT "make_umi_fastq.pl: sample was determined to be 5' 10x paired-end experiment!\n";
-	} else { 
-		print STDOUT "make_umi_fastq.pl: sample was determined to be 3'/5' 10x single-end experiment!\n"; 
-	} 
+  ## now do the processing. We keep only the BC+UMI part of the barcode read
+  ## this would lose some biological info in case of PE 5' experiments, but I think it should not matter this much for bacterial reads
+  if ($paired) { 
+    open BAM,"samtools view -\@4 -f132 $bam |" or die "ERROR: failed to open the bam file using samtools!";
+  } else { 
+    open BAM,"samtools view -\@4 -f4 $bam |" or die "ERROR: failed to open the bam file using samtools!";
+  }
 
-	## now do the processing. We keep only the BC+UMI part of the barcode read
-	## this would lose some biological info in case of PE 5' experiments, but I think it should not matter this much for bacterial reads
-	if ($paired) { 
-		open BAM,"samtools view -\@4 -f132 $bam |" or die "ERROR: failed to open the bam file using samtools!";
-	} else { 
-		open BAM,"samtools view -\@4 -f4 $bam |" or die "ERROR: failed to open the bam file using samtools!";
-	}
-
-	while (<BAM>) { 
-    my ($rname,$bc,$umi,$seq,$qual) = ('') x 5;
+  my ($rname,$bc,$umi,$seq,$qual) = ('') x 5;
+  my ($passed, $discarded) = (0) x 2; 
+  while (<BAM>) { 
     $rname = (split /\t/)[0];
     $seq   = (split /\t/)[9];
     $qual  = (split /\t/)[10];
-    m/CR:\w:(\w+)\t.*UR:\w:(\w+)\t/;
-    $bc = $1;
-    $umi = $2;
-    printf OUT "@%s_%s_%s\n%s\n+\n%s\n",$rname,$bc,$umi,$seq,$qual;
-	} 
-	
-	close BAM;
+    if (m/CB:\w:([A-Z]+).*UB:\w:([A-Z]+)/) { 
+      $bc = $1;
+      $umi = $2;
+    } elsif (m/CR:\w:([A-Z]+).*UR:\w:([A-Z]+)/) {
+      $bc = $1; 
+      $umi = $2; 
+    } else { 
+      print STDERR "make_umi_fastq.pl: WARNING - no barcode/umi tag found for read $rname! Please investigate..\n"; 
+    } 
+
+    ## chech if read has too much homopolymers 
+    if (! low_complexity($seq)) { 
+      printf OUT "%s_%s_%s\n%s\n+\n%s\n",$rname,$bc,$umi,$seq,$qual;
+      $passed++; 
+    } else {
+      $discarded++; 
+    }
+  } 
+  print STDOUT "make_umi_fastq.pl: making UMI-tools formatted reads: outputted $passed reads, discarded $discarded reads because of extensive homopolymers ..\n"; 
+  close BAM;
 } 
 
 close OUT; 
@@ -187,8 +197,8 @@ sub low_complexity {
     if ($ltr eq $top_ltr) {
       $cur_substr++;
       $max_substr = ($cur_substr > $max_substr) ? $cur_substr : $max_substr;
-	} elsif ($ltr ne $top_ltr && $cur_substr == 0) {
-	  next; ## don't count mismatches if we are not in the homopolymer yet
+    } elsif ($ltr ne $top_ltr && $cur_substr == 0) {
+      next; ## don't count mismatches if we are not in the homopolymer yet
     } elsif ($ltr ne $top_ltr && $mismatch == 0) {
       $mismatch = 1;
       $cur_substr++;
